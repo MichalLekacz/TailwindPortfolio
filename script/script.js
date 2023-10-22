f28a21fa23e03abcfa79229965e02b9d62c6c6e1
MichalLekacz <87401492+MichalLekacz@users.noreply.github.com>
2023-10-22 14:16:38 +0100

    hamburger
    hamburger and footer

--- script/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-    });
-</script>
